740e6920dedd1c70805fb3571027ac4b6ce80ca9
Anthony Mochel <tony.mochel@gmail.com>
2020-03-27 15:32:31 +0100

    build(): add firebase - google analytics

---
+++ asset/script/firebase.js
@@ -0,0 +1,15 @@
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAS1ou72KWaGWTnUCpXCb4f6z-XzweglZY",
+    authDomain: "portfolio-github-272413.firebaseapp.com",
+    databaseURL: "https://portfolio-github-272413.firebaseio.com",
+    projectId: "portfolio-github-272413",
+    storageBucket: "portfolio-github-272413.appspot.com",
+    messagingSenderId: "985440745532",
+    appId: "1:985440745532:web:6bfc85e2da98882ff72b39",
+    measurementId: "G-SE8BY31SM4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
